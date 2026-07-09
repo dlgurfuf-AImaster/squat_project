bfa3c39c2d5be2748832ec7f409cce53a1807faa
dlgurfuf-AImaster <dlgurfuf@gmail.com>
2026-07-09 20:57:36 +0900

    feat: LoginResponse.java DTO 생성. JWT 토큰을 위하여

---
+++ server/src/main/java/com/squat/server/dto/LoginResponse.java
@@ -0,0 +1,14 @@
+package com.squat.server.dto;
+
+public class LoginResponse {
+    private String token;
+    private String name;
+
+    public LoginResponse(String token, String name) {
+        this.token = token;
+        this.name = name;
+    }
+
+    public String getToken() { return token; }
+    public String getName() { return name; }
+}
